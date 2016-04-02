50cfba72deff6f25c59c214d8e12250889531a50
web-storm <s-t-o-r-m@list.ru>
2016-04-02 13:38:36 +0300

    #3
    Adding a timer

---
+++ IB23-2/Controllers/TimerController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IB23_2.Controllers
+{
+    public class TimerController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
